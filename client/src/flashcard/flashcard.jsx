ac83fd554e5ed76dac6f6662a00d08c2fb6a9b54
GrantCanty <gc933815@ohio.edu>
2023-02-23 23:58:10 -0700

    updated flashcard

--- client/src/flashcard/flashcard.jsx
+++ client/src/flashcard/flashcard.jsx
@@ -1,10 +1,21 @@
+import React from 'react'
 import './flashcard.css'
 
-const Flashcard = ({deckInfo, deckLength}) => {
+const Flashcard = ({deckInfo, cardPos, setCardPos, showTitle, toggleShowTitle}) => {
+
+    /*const [showTitle, setShowTitle] = React.useState(true)
+    function toggleShowTitle() {
+        setShowTitle(!showTitle)
+
+    }*/
+
+    const keys = [...deckInfo.keys()]
 
     return (
-        <div className="flashcard">
-            <h2>{deckLength}</h2>
+        <div className="flashcard" onClick={toggleShowTitle}>
+            { showTitle ? <h2>{keys[cardPos]}</h2> : <h2>{deckInfo.get(keys[cardPos])}</h2> }
+            <button className='new-card' onClick={(e) => {e.stopPropagation(); setCardPos(-1)}} >-</button>
+            <button className='new-card' onClick={(e) => {e.stopPropagation(); setCardPos(1)}}  >+</button>
         </div>
     )
 }
--- client/src/flashcard/flashcardParent.jsx
+++ client/src/flashcard/flashcardParent.jsx
@@ -1,15 +1,38 @@
+import React from "react";
 import Flashcard from "./flashcard";
 
 
 function FlashcardParent(props) {
+    const [showTitle, setShowTitle] = React.useState(true)
+
+    function toggleShowTitle() {
+        setShowTitle(!showTitle)
+
+    }
     
-    
+
     const keys = [...props.deckInfo.keys()]
     const deckLength = keys.length
 
+    const [cardPos, setCardPos] = React.useState(Number)
+
+    function newCardPos(num) {
+        if (num === -1 && cardPos > 0) {
+            setCardPos(cardPos + num)
+            setShowTitle(true)
+        } 
+        if (num === 1 && cardPos < deckLength -1) {
+            setCardPos(cardPos + num)
+            setShowTitle(true)
+        }
+    }
+
+    
+
     return (
         <div >
-            <Flashcard deckInfo={props.deckInfo} deckLength={deckLength} />
+
+            <Flashcard deckInfo={props.deckInfo} cardPos={cardPos} setCardPos={newCardPos} showTitle={showTitle} toggleShowTitle={toggleShowTitle} />
         </div>
     )
 
